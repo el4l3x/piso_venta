7cb9b3c950a4910cc0394c87827a1082c4f3af62
el4l3x <davidlopez.fya@gmail.com>
2024-01-01 17:02:08 -0400

    model --all Dispatch + migration

---
+++ app/Http/Controllers/DispatchController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dispatch;
+use App\Http\Requests\StoreDispatchRequest;
+use App\Http\Requests\UpdateDispatchRequest;
+
+class DispatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDispatchRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dispatch $dispatch)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dispatch $dispatch)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDispatchRequest $request, Dispatch $dispatch)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dispatch $dispatch)
+    {
+        //
+    }
+}
